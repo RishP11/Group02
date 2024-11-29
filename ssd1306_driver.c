f537a95f81a1129abe3b0720136378f9d23d01b7
Rishabh Pomaje <97334879+RishP11@users.noreply.github.com>
2024-11-29 12:18:20 +0530

    "Added comments"

--- ssd1306_driver.c
+++ ssd1306_driver.c
@@ -5,7 +5,9 @@
 
 void I2C_enable( void )
 {
-  // Setting up all the clocks to
+  /*
+  Function to Enable clocks to GPIO, modules, etc.
+  */
   SYSCTL_RCGCI2C_R |= 0x8 ;                               // I2C module 3
   SYSCTL_RCGCGPIO_R |= 0x00008 ;                          // GPIO Port D
 }
@@ -23,62 +25,71 @@ void PORTD_init( void )
 
 void I2C3_setup( void )
 {
+  /*
+  Function to setup the I2C3 module to interface with the oled
+  */
   // Time period register value calculation
-  int SCL_HP = 6 ;
+  int SCL_HP = 6 ;                  
   int SCL_LP = 4 ;
   int SCL_CLK = 400000 ;
   int TPR = (1.0 * SYSCLK_HZ / (2 * (SCL_HP + SCL_LP) * SCL_CLK)) - 1 ;
-  I2C3_MCR_R = 0x10 ;                                     // |GFE|SFE|MFE|.|.|.|LPBK|
+  I2C3_MCR_R = 0x10 ;                                     // |GFE|SFE|MFE|.|.|.|LPBK| - Configuration Register
   I2C3_MTPR_R = TPR ;                                     // Time Period Register
   I2C3_MSA_R = 0x78 ;                                     // |.|.|.|.|.|.|.|.|R/W'|
 }
 
 void I2C3_Tx( uint8_t controlByte, uint8_t dataByte )
 {
-  I2C3_MDR_R = controlByte ;  // Place the data
-  I2C3_MCS_R = 0x03 ;                 // Start and Run
-  while(I2C3_MCS_R & 0x01) ; // Wait until busy
-  I2C3_MDR_R = dataByte ;     // Data to GDDRAM
-  I2C3_MCS_R = 0x05 ;             // Run and Stop
-  while(I2C3_MCS_R & 0x01) ; // Wait until busy
+  /*
+  Function to transmit data frames to the oled
+  */
+  I2C3_MDR_R = controlByte ;                              // Place the data
+  I2C3_MCS_R = 0x03 ;                                     // Start and Run
+  while(I2C3_MCS_R & 0x01) ;                              // Wait until busy
+  I2C3_MDR_R = dataByte ;                                 // Data Content
+  I2C3_MCS_R = 0x05 ;                                     // Run and Stop
+  while(I2C3_MCS_R & 0x01) ;                              // Wait until busy
 }
 
 void oled_init( void )
 {
-  I2C3_Tx(OLED_COMMAND, OLED_DISPLAY_OFF);         // Display OFF
-  I2C3_Tx(OLED_COMMAND, OLED_MEMORY_MODE);     // Set Memory Addressing Mode
-  I2C3_Tx(OLED_COMMAND, 0x02);           // Page Addressing Mode
-  I2C3_Tx(OLED_COMMAND, 0xB0);      // Set Page Start Address
-  I2C3_Tx(OLED_COMMAND, OLED_COM_SCAN_DIR);        // COM Output Scan Direction
-  I2C3_Tx(OLED_COMMAND, OLED_SET_LOW_COLUMN);      // Set Low Column Address
-  I2C3_Tx(OLED_COMMAND, OLED_SET_HIGH_COLUMN);     // Set High Column Address
-  I2C3_Tx(OLED_COMMAND, OLED_SET_START_LINE);      // Set Start Line Address
-  I2C3_Tx(OLED_COMMAND, OLED_SET_CONTRAST);        // Set Contrast Control Register
-  I2C3_Tx(OLED_COMMAND, 0x77);                     // Set Contrast Value
-  I2C3_Tx(OLED_COMMAND, OLED_SEG_REMAP);           // Set Segment Re-map
-  I2C3_Tx(OLED_COMMAND, OLED_NORMAL_DISPLAY);      // Set Normal Display
-  I2C3_Tx(OLED_COMMAND, OLED_SET_MULTIPLEX);       // Set Multiplex Ratio
-  I2C3_Tx(OLED_COMMAND, OLED_MULTIPLEX_RATIO);     // Multiplex Ratio Value
-  I2C3_Tx(OLED_COMMAND, OLED_RAM2DISPLAY);         // Output RAM to Display
-  I2C3_Tx(OLED_COMMAND, OLED_SET_OFFSET);          // Set Display Offset
-  I2C3_Tx(OLED_COMMAND, 0x00);        // Offset Value
-  I2C3_Tx(OLED_COMMAND, OLED_SET_CLOCK_DIV);       // Set Display Clock Divide Ratio
-  I2C3_Tx(OLED_COMMAND, OLED_CLOCK_RATIO);         // Clock Divide Ratio Value
-  I2C3_Tx(OLED_COMMAND, OLED_SET_PRECHARGE);       // Set Pre-charge Period
-  I2C3_Tx(OLED_COMMAND, OLED_PRECHARGE_VALUE);     // Pre-charge Value
-  I2C3_Tx(OLED_COMMAND, OLED_SET_COM_PINS);        // Set COM Pins Hardware Configuration
-  I2C3_Tx(OLED_COMMAND, OLED_COM_PINS_VALUE);      // Hardware Configuration Value
-  I2C3_Tx(OLED_COMMAND, OLED_SET_VCOMH);           // Set VCOMH
-  I2C3_Tx(OLED_COMMAND, OLED_VCOMH_VALUE);         // VCOMH Value
-  I2C3_Tx(OLED_COMMAND, OLED_CHARGE_PUMP);         // Enable Charge Pump Regulator
-  I2C3_Tx(OLED_COMMAND, OLED_CHARGE_PUMP_ENABLE);  // Charge Pump Regulator Value
-  I2C3_Tx(OLED_COMMAND, OLED_DISPLAY_ON);          // Display ON
+  /*
+  Function to initialize the display
+  */
+  I2C3_Tx(OLED_COMMAND, OLED_DISPLAY_OFF);                // Display OFF
+  I2C3_Tx(OLED_COMMAND, OLED_MEMORY_MODE);                // Set Memory Addressing Mode
+  I2C3_Tx(OLED_COMMAND, 0x02);                            // Page Addressing Mode
+  I2C3_Tx(OLED_COMMAND, 0xB0);                            // Set Page Start Address
+  I2C3_Tx(OLED_COMMAND, OLED_COM_SCAN_DIR);               // COM Output Scan Direction
+  I2C3_Tx(OLED_COMMAND, OLED_SET_LOW_COLUMN);             // Set Low Column Address
+  I2C3_Tx(OLED_COMMAND, OLED_SET_HIGH_COLUMN);            // Set High Column Address
+  I2C3_Tx(OLED_COMMAND, OLED_SET_START_LINE);             // Set Start Line Address
+  I2C3_Tx(OLED_COMMAND, OLED_SET_CONTRAST);               // Set Contrast Control Register
+  I2C3_Tx(OLED_COMMAND, 0xFF);                            // Set Contrast Value
+  I2C3_Tx(OLED_COMMAND, OLED_SEG_REMAP);                  // Set Segment Re-map
+  I2C3_Tx(OLED_COMMAND, OLED_NORMAL_DISPLAY);             // Set Normal Display
+  I2C3_Tx(OLED_COMMAND, OLED_SET_MULTIPLEX);              // Set Multiplex Ratio
+  I2C3_Tx(OLED_COMMAND, OLED_MULTIPLEX_RATIO);            // Multiplex Ratio Value
+  I2C3_Tx(OLED_COMMAND, OLED_RAM2DISPLAY);                // Output RAM to Display
+  I2C3_Tx(OLED_COMMAND, OLED_SET_OFFSET);                 // Set Display Offset
+  I2C3_Tx(OLED_COMMAND, 0x00);                            // Offset Value
+  I2C3_Tx(OLED_COMMAND, OLED_SET_CLOCK_DIV);              // Set Display Clock Divide Ratio
+  I2C3_Tx(OLED_COMMAND, OLED_CLOCK_RATIO);                // Clock Divide Ratio Value
+  I2C3_Tx(OLED_COMMAND, OLED_SET_PRECHARGE);              // Set Pre-charge Period
+  I2C3_Tx(OLED_COMMAND, OLED_PRECHARGE_VALUE);            // Pre-charge Value
+  I2C3_Tx(OLED_COMMAND, OLED_SET_COM_PINS);               // Set COM Pins Hardware Configuration
+  I2C3_Tx(OLED_COMMAND, OLED_COM_PINS_VALUE);             // Hardware Configuration Value
+  I2C3_Tx(OLED_COMMAND, OLED_SET_VCOMH);                  // Set VCOMH
+  I2C3_Tx(OLED_COMMAND, OLED_VCOMH_VALUE);                // VCOMH Value
+  I2C3_Tx(OLED_COMMAND, OLED_CHARGE_PUMP);                // Enable Charge Pump Regulator
+  I2C3_Tx(OLED_COMMAND, OLED_CHARGE_PUMP_ENABLE);         // Charge Pump Regulator Value
+  I2C3_Tx(OLED_COMMAND, OLED_DISPLAY_ON);                 // Display ON
 }
 
 void oledClear( void )
 {
   /*
-  Function that clears the complete screen :: Under development
+  Function that clears the complete screen
   */
   int i ;
   // Page 0 :
@@ -176,33 +187,36 @@ void oledPrintStr(char charArray[])
     I2C3_Tx(OLED_DATA, ASCII[(int) ' ' - diff][i]) ;
     num_char += 1 ;
   } 
-  int blank_col_fills = 128 -  (6 * inputStrLen) ;
+  int blank_col_fills = 128 -  (6 * inputStrLen) ;              // 5+1 = Character width + Space
   while(blank_col_fills > 0){
-    I2C3_Tx(OLED_DATA, 0x00) ; // Blank data 
+    I2C3_Tx(OLED_DATA, 0x00) ;                                  // Blank data 
     blank_col_fills -= 1 ;
   }
 }
 
 void num2str(float num, char *str, int precision) {
+    /*
+    Function to convert floating pt number to string 
+    */
     // Handle negative numbers
     if (num < 0) {
         *str++ = '-';
-        num = -num;  // Make the number positive for further processing
+        num = -num;                                           // Make the number positive for further processing
     }
 
     // Get the integer part of the float
     int integerPart = (int) num;
-    num -= integerPart;  // Remove the integer part, leaving the fractional part
+    num -= integerPart;                                     // Remove the integer part, leaving the fractional part
 
     // Convert the integer part to string
-    char temp[50];  // Temporary array for the integer part
+    char temp[50];                                          // Temporary array for the integer part
     int i = 0;
     int j ;
     if (integerPart == 0) {
-        temp[i++] = '0';  // Special case for zero
+        temp[i++] = '0';                                    // Special case for zero
     } else {
         while (integerPart > 0) {
-            temp[i++] = (integerPart % 10) + '0';  // Convert digit to character
+            temp[i++] = (integerPart % 10) + '0';           // Convert digit to character
             integerPart /= 10;
         }
     }
